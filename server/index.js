661f760a3da5334809905050db6757f3df6fe6cf
Gary Smith <garysmith193358@gmail.com>
2023-12-05 18:32:58 -0500

    working on getting data from backend

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const shortUrl = require("./models/urls");
 
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 
-mongoose.connect("mongodb://localhost/urlShortener")
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 
 app.get('/', (req, res) => {
@@ -13,12 +18,19 @@ app.get('/', (req, res) => {
 })
 
 app.post('/shortenUrl', async (req, res) => {
-  const foundUrl = await shortUrl.findOne({ fullUrl: req.body.fullUrl });
+  try {
+    //left off here
+    const foundUrl = await shortUrl.findOne({ fullUrl: req.body.fullUrl });
 
-  if (foundUrl) res.send(foundUrl.shortUrl)
+    if (foundUrl) res.send(foundUrl.shortUrl)
+  
+    const shortenedUrl = await shortUrl.create({fullUrl: req.body.fullUrl})
+    res.send(shortenedUrl.shortUrl)
+  }
 
-  const shortenedUrl = await shortUrl.create({fullUrl: req.body.fullUrl})
-  res.send(shortenedUrl.shortUrl)
+  catch (err) {
+    console.log(err)
+  }
 })
 
 const PORT = process.env.PORT || 8080;
--- server/models/urls.js
+++ server/models/urls.js
@@ -1,22 +1,23 @@
 const mongoose = require("mongoose");
-const { nanoid } = require("nanoid");
+const { model } = require("mongoose");
+const { nanoid } = require('nanoid');
 const { Schema } = mongoose;
 
 const urlSchema = new Schema({
-  key: nanoid(),
   fullUrl: {
     type: String,
     required: true
   },
   
   shortUrl: {
-   type: String,
-   required: true,
-   default: nanoid(6)
+    type: String,
+    required: true,
+    default: nanoid(6)
   }
-})
+});
 
-urlModel = mongoose.model("url", urlSchema);
+const urlModel = model("url", urlSchema);
+
 module.exports = {
   urlModel
-}
+};
