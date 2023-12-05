0fa52a293c466836678bc85dce518c4f01b95e3d
Gary Smith <garysmith193358@gmail.com>
2023-12-05 16:17:02 -0500

    created post request route for shortened urls

--- server/index.js
+++ server/index.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const urlModel = require("./models/urls");
+const shortUrl = require("./models/urls");
 
+app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect("mongodb://localhost/urlShortener")
 
 
 app.get('/', (req, res) => {
     res.send('Hello!')
+})
+
+app.post('/shortenUrl', async (req, res) => {
+  const foundUrl = await shortUrl.findOne({ fullUrl: req.body.fullUrl });
+
+  if (foundUrl) res.send(foundUrl.shortUrl)
+
+  const shortenedUrl = await shortUrl.create({fullUrl: req.body.fullUrl})
+  res.send(shortenedUrl.shortUrl)
 })
 
 const PORT = process.env.PORT || 8080;
